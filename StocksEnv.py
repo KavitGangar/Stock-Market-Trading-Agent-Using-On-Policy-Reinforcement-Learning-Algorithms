24049e7df2c4eb627971fc39ced5da70fd343afa
shreyas2398 <47748881+shreyas2398@users.noreply.github.com>
2019-02-26 19:46:22 +0530

    Update StocksEnv.py

--- StocksEnv.py
+++ StocksEnv.py
@@ -212,9 +212,9 @@ class StocksEnv(gym.Env):
         self.stride = self.max_stride # no longer varying it
         
         
-        self.state[0] = 10 #random.randint(0,20)
-        self.state[1] = 10 #random.randint(0,20)
-        self.state[2] = 2000 #random.randint(100,2000)
+        self.state[0] = random.randint(0,20)
+        self.state[1] = random.randint(0,20)
+        self.state[2] = random.randint(100,2000)
         self.state[3] = apl_open[self.cur_timestep]
         self.state[4] = msf_open[self.cur_timestep]
         self.starting_portfolio_value = self.portfolio_value()
