b059fcf1d7e75dbd5b471d4fa0498a023cc1d6dc
shreyas2398 <47748881+shreyas2398@users.noreply.github.com>
2019-02-28 14:24:55 +0530

    Update StocksEnv.py

--- StocksEnv.py
+++ StocksEnv.py
@@ -41,7 +41,7 @@ class StocksEnv(gym.Env):
         
         #===
         
-        self.state = np.zeros(4)
+        self.state = np.zeros(5)
         
         self.starting_cash = 2000
         self.buycount=0
@@ -165,7 +165,7 @@ class StocksEnv(gym.Env):
         return retval
 
     def reset(self):
-        self.state = np.zeros(4)
+        self.state = np.zeros(5)
         self.starting_cash = 200
         self.cur_timestep = random.randint(0,100)
         self.starting_point = self.cur_timestep
