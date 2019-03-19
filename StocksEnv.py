4d587d873038af548ef3feb8d9aee4fe4ac525e2
KavitGangar <39722464+KavitGangar@users.noreply.github.com>
2019-03-19 17:39:48 +0530

    Update StocksEnv.py

--- StocksEnv.py
+++ StocksEnv.py
@@ -99,7 +99,7 @@ class StocksEnv(gym.Env):
         
         
         
-        if action[0] == 1:
+        if action[0] == 2:
             if action[1] > self.state[0]:
                 self.nothingpseudo+=1
                 new_state = [self.state[0], self.state[1] ,self.next_opening_price(), \
@@ -125,7 +125,7 @@ class StocksEnv(gym.Env):
         
         
         
-        if action[0] == 2:
+        if action[0] == 1:
             self.nothing += 1
             new_state = [self.state[0], self.state[1] ,self.next_opening_price(), \
                      *self.five_day_window(),self.state[13],self.next_open_price(self.state[0])]
