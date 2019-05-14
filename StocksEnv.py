847aaa901e48ff502b7df148d6cd10cb7f71889d
shreyas2398 <47748881+shreyas2398@users.noreply.github.com>
2019-05-15 04:23:21 +0530

    Update StocksEnv.py

--- StocksEnv.py
+++ StocksEnv.py
@@ -8,15 +8,15 @@ import random
 import matplotlib.pyplot as plt
 
 import pickle
-with open("./aplmsfopenclose.pkl", "rb") as f:
+with open("./mystocks.pkl", "rb") as f:
     d = pickle.load(f)
     
 action_f = open('./numpy.txt', 'a')
 profit_f = open('./profit.txt', 'a')
 
 
-apl_open = d["mo"]
-apl_close = d["mc"]
+apl_open = d["trv_open"]
+apl_close = d["trv_close"]
 msf_open = d["mo"]
 msf_close = d["mc"]
 
